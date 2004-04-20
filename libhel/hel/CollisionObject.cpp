dde68178d7f35bbe52de6bcb1e28d03d5d0cb5c3
Terry Hendrix II <thendrix@users.noreply.github.com>
2004-04-20 06:36:09 +0000

    Commited libhel to CVS

---
+++ libhel/hel/CollisionObject.cpp
@@ -0,0 +1,79 @@
+/* -*- Mode: C++; tab-width: 3; indent-tabs-mode: t; c-basic-offset: 3 -*- */
+/*================================================================
+ * 
+ * Project : Hel
+ * Author  : Terry 'Mongoose' Hendrix II
+ * Website : http://www.westga.edu/~stu7440/
+ * Email   : stu7440@westga.edu
+ * Object  : CollisionObject
+ * License : No use w/o permission (C) 2003 Mongoose
+ * Comments: This is the magical collision object class 
+ *
+ *
+ *           This file was generated using Mongoose's C++ 
+ *           template generator script.  <stu7440@westga.edu>
+ * 
+ *-- History ------------------------------------------------- 
+ *
+ * 2003.06.07:
+ * Mongoose - Created
+ =================================================================*/
+
+#include "CollisionObject.h"
+
+
+////////////////////////////////////////////////////////////
+// Constructors
+////////////////////////////////////////////////////////////
+
+CollisionObject::CollisionObject()
+{
+}
+
+
+CollisionObject::~CollisionObject()
+{
+}
+
+
+////////////////////////////////////////////////////////////
+// Public Accessors
+////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////
+// Public Mutators
+////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////
+// Private Accessors
+////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////
+// Private Mutators
+////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////
+// Unit Test code
+////////////////////////////////////////////////////////////
+
+#ifdef UNIT_TEST_COLLISIONOBJECT
+int runCollisionObjectUnitTest(int argc, char *argv[])
+{
+	CollisionObject test;
+
+	return 0;
+}
+
+
+int main(int argc, char *argv[])
+{
+	printf("[CollisionObject class test]\n");
+	runCollisionObjectUnitTest(argc, argv);
+
+	return 0;
+}
+#endif
