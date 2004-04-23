173b9324cc77aa69044653029eaf21a883256f55
Terry Hendrix II <hendrix.terry@gmail.com>
2004-04-23 05:12:46 +0000

    Added some older midgard models; GRNModel is a newer one class version of the GRNSystem, but it's not finished

---
+++ egg_plugins/src/UTMesh.cpp
@@ -0,0 +1,79 @@
+/* -*- Mode: C++; tab-width: 3; indent-tabs-mode: t; c-basic-offset: 3 -*- */
+/*================================================================
+ * 
+ * Project : Midgard
+ * Author  : Terry 'Mongoose' Hendrix II
+ * Website : http://www.westga.edu/~stu7440/
+ * Email   : stu7440@westga.edu
+ * Object  : UTMesh
+ * License : No use w/o permission (C) 2004 Mongoose
+ * Comments: This is the Unreal mesh classes
+ *
+ *
+ *           This file was generated using Mongoose's C++ 
+ *           template generator script.  <stu7440@westga.edu>
+ * 
+ *-- History ------------------------------------------------- 
+ *
+ * 2004.03.04:
+ * Mongoose - Created
+ =================================================================*/
+
+#include "UTMesh.h"
+
+
+////////////////////////////////////////////////////////////
+// Constructors
+////////////////////////////////////////////////////////////
+
+UTMesh::UTMesh()
+{
+}
+
+
+UTMesh::~UTMesh()
+{
+}
+
+
+////////////////////////////////////////////////////////////
+// Public Accessors
+////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////
+// Public Mutators
+////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////
+// Private Accessors
+////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////
+// Private Mutators
+////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////
+// Unit Test code
+////////////////////////////////////////////////////////////
+
+#ifdef UNIT_TEST_UTMESH
+int runUTMeshUnitTest(int argc, char *argv[])
+{
+	return 0;
+}
+
+
+int main(int argc, char *argv[])
+{
+	UTMesh test;
+
+
+	printf("[UTMesh class test]\n");
+
+	return runUTMeshUnitTest(argc, argv);
+}
+#endif
