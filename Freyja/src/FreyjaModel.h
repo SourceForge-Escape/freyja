b4d1dea3502de296bc9af2c5039e07cf1402fa34
Terry Hendrix II <hendrix.terry@gmail.com>
2004-12-08 00:35:29 +0000

    test patch system

--- Freyja/src/FreyjaModel.h
+++ Freyja/src/FreyjaModel.h
@@ -32,6 +32,7 @@
 
 #include <freyja8/EggPlugin.h>
 #include <freyja8/Egg.h>
+#include <hel/Vector3d.h>
 #include <mstl/Vector.h>
 #include "Light.h"
 #include "freyja_events.h"
@@ -105,6 +106,124 @@ public:
 };
 
 
+class BezierPatch
+{
+public:
+	BezierPatch()
+	{
+		texture = 0;
+		displayList = 0;
+		x = y = 0;
+
+		reset();
+	}
+
+	void generatePolygonMesh(int divs)
+	{
+		int u = 0, v;
+		vec_t py, px, pyold; 
+		Vector3d temp[4];
+		Vector3d *last = new Vector3d[divs+1];  /* Array Of Points To Mark
+												   The First Line Of Polygons */
+
+		temp[0] = control[0][3]; // The First Derived Curve (Along X-Axis)
+		temp[1] = control[1][3];
+		temp[2] = control[2][3];
+		temp[3] = control[3][3];
+
+		for (v = 0; v <= divs; ++v)
+		{
+			// Create The First Line Of Points
+			px = ((vec_t)v)/((vec_t)divs);		// Percent Along Y-Axis
+			/* Use The 4 Points From The Derived Curve To 
+			 * Calculate The Points Along That Curve */
+			last[v] = solveBernstein(px, temp);
+		}
+
+		vertices.erase();
+		texcoords.erase();
+
+		for (u = 1; u <= divs; ++u) 
+		{
+			py    = ((float)u)/((float)divs);		// Percent Along Y-Axis
+			pyold = ((float)u-1.0f)/((float)divs);	// Percent Along Old Y Axis
+
+			// Calculate New Bezier Points
+			temp[0] = solveBernstein(py, control[0]);
+			temp[1] = solveBernstein(py, control[1]);
+			temp[2] = solveBernstein(py, control[2]);
+			temp[3] = solveBernstein(py, control[3]);
+
+			for (v = 0; v <= divs; ++v)
+			{
+				px = ((float)v)/((float)divs);		// Percent Along The X-Axis
+				
+				vertices.pushBack(new Vector3d(last[v]));
+				texcoords.pushBack(new Vector3d(pyold, px, 0));
+
+				last[v] = solveBernstein(px, temp);
+				vertices.pushBack(new Vector3d(last[v]));
+				texcoords.pushBack(new Vector3d(py, px, 0));
+			}
+		}
+
+		delete [] last;
+	}
+
+	void reset()
+	{
+		unsigned int i, j;
+
+
+		control[0][0] = Vector3d(-0.75,	0.50, -0.75);
+		control[0][1] = Vector3d(-0.25,	0.00, -0.75);
+		control[0][2] = Vector3d( 0.25,	0.00, -0.75);
+		control[0][3] = Vector3d( 0.75,	0.50, -0.75);
+		control[1][0] = Vector3d(-0.75,	0.75, -0.25);
+		control[1][1] = Vector3d(-0.25,	0.50, -0.25);
+		control[1][2] = Vector3d( 0.25,	0.50, -0.25);
+		control[1][3] = Vector3d( 0.75,	0.75, -0.25);
+		control[2][0] = Vector3d(-0.75,	0.00,  0.25);
+		control[2][1] = Vector3d(-0.25,	0.50,  0.25);
+		control[2][2] = Vector3d( 0.25,	0.50,  0.25);
+		control[2][3] = Vector3d( 0.75,	0.00,  0.25);
+		control[3][0] = Vector3d(-0.75,	0.50,  0.75);
+		control[3][1] = Vector3d(-0.25,	1.00,  0.75);
+		control[3][2] = Vector3d( 0.25,	1.00,  0.75);
+		control[3][3] = Vector3d( 0.75,	0.50,  0.75);
+		
+		for (i = 0; i < 4; ++i)
+		{
+			for(j = 0; j < 4; ++j)
+				control[i][j] *= 10.0f;
+		}
+
+		vertices.erase();
+		texcoords.erase();
+	}
+
+	Vector3d solveBernstein(vec_t u, Vector3d *p)
+	{
+		Vector3d a, b, c, d, r;
+
+		a = p[0] * pow(u, 3);
+		b = p[1] * 3*pow(u, 2)*(1-u);
+		c = p[2] * 3*u*pow((1-u), 2);
+		d = p[3] * pow((1-u), 3);
+
+		r = (a + b) + (c + d);
+
+		return r;
+	}
+
+	Vector3d control[4][4];
+
+	Vector<Vector3d *> vertices;
+	Vector<Vector3d *> texcoords;
+	unsigned int texture;
+	unsigned int displayList;
+	unsigned int x, y;
+};
 
 
 class FreyjaEgg : public Egg
@@ -592,6 +711,10 @@ public:
 	void setSceneTranslation(vec_t x, vec_t y, vec_t z);
 
 
+	void movePatchControlPoint(float xx, float yy);
+	void selectPatchControlPoint(float xx, float yy);
+
+
 	////////////////////////////////////////////////////////////////////////
 
 	void Clear();
@@ -650,6 +773,8 @@ public:
 	bool pasteSelectedMesh();
 
 	void deleteAnimationFrame(unsigned int frame);
+
+	bool pasteSelectedPatch();
 
 
 	///////////////////////////////////////////////////////
