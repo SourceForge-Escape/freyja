0876251e73c47885b8b6c349117f891133e0cd66
mongoose <mongoose@607110db-3bd9-0310-8033-d7c559c36a9d>
2005-07-09 04:37:15 +0000

    shuffling more class names

---
+++ branches/dancer/libfreyja/src/Animation.cpp
@@ -0,0 +1,78 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+/*===========================================================================
+ * 
+ * Project : freyja
+ * Author  : Terry 'Mongoose' Hendrix II
+ * Website : http://www.icculus.org/~mongoose/
+ * Email   : mongoose@icculus.org
+ * Object  : Animation
+ * License : No use w/o permission (C) 2005 Mongoose
+ * Comments: This is the animation class ( holds keyframes )
+ *
+ *
+ *           This file was generated using Mongoose's C++ 
+ *           template generator script.  <mongoose@icculus.org>
+ * 
+ *-- History ------------------------------------------------- 
+ *
+ * 2005.07.08:
+ * Mongoose - Created
+ ==========================================================================*/
+
+#include "Animation.h"
+
+
+////////////////////////////////////////////////////////////
+// Constructors
+////////////////////////////////////////////////////////////
+
+Animation::Animation()
+{
+}
+
+
+Animation::~Animation()
+{
+}
+
+
+////////////////////////////////////////////////////////////
+// Public Accessors
+////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////
+// Public Mutators
+////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////
+// Private Accessors
+////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////
+// Private Mutators
+////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////
+// Unit Test code
+////////////////////////////////////////////////////////////
+
+#ifdef UNIT_TEST_ANIMATION
+int runAnimationUnitTest(int argc, char *argv[])
+{
+	Animation test;
+
+	return 0;
+}
+
+
+int main(int argc, char *argv[])
+{
+	printf("[Animation class test]\n");
+
+	return runAnimationUnitTest(argc, argv);
+}
+#endif
---
+++ branches/dancer/libfreyja/src/KeyFrameLight.cpp
@@ -0,0 +1,78 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+/*===========================================================================
+ * 
+ * Project : freyja
+ * Author  : Terry 'Mongoose' Hendrix II
+ * Website : http://www.icculus.org/~mongoose/
+ * Email   : mongoose@icculus.org
+ * Object  : KeyFrameLight
+ * License : No use w/o permission (C) 2005 Mongoose
+ * Comments: Light keyframe
+ *
+ *
+ *           This file was generated using Mongoose's C++ 
+ *           template generator script.  <mongoose@icculus.org>
+ * 
+ *-- History ------------------------------------------------- 
+ *
+ * 2005.07.08:
+ * Mongoose - Created
+ ==========================================================================*/
+
+#include "KeyFrameLight.h"
+
+
+////////////////////////////////////////////////////////////
+// Constructors
+////////////////////////////////////////////////////////////
+
+KeyFrameLight::KeyFrameLight()
+{
+}
+
+
+KeyFrameLight::~KeyFrameLight()
+{
+}
+
+
+////////////////////////////////////////////////////////////
+// Public Accessors
+////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////
+// Public Mutators
+////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////
+// Private Accessors
+////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////
+// Private Mutators
+////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////
+// Unit Test code
+////////////////////////////////////////////////////////////
+
+#ifdef UNIT_TEST_KEYFRAMELIGHT
+int runKeyFrameLightUnitTest(int argc, char *argv[])
+{
+	KeyFrameLight test;
+
+	return 0;
+}
+
+
+int main(int argc, char *argv[])
+{
+	printf("[KeyFrameLight class test]\n");
+
+	return runKeyFrameLightUnitTest(argc, argv);
+}
+#endif
