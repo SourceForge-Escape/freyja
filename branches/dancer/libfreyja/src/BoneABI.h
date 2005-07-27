a4794f836435437d4cb44d119062b32d9b7e62fd
mongoose <mongoose@607110db-3bd9-0310-8033-d7c559c36a9d>
2005-07-27 23:09:10 +0000

    power outage resync

---
+++ branches/dancer/libfreyja/src/BoneABI.h
@@ -0,0 +1,169 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+/*===========================================================================
+ * 
+ * Project : freyja
+ * Author  : Terry 'Mongoose' Hendrix II
+ * Website : http://icculus.org/freyja
+ * Email   : mongoose@icculus.org
+ * Object  : 
+ * License : No use w/o permission (C) 2005 Mongoose
+ * Comments: This is the bone data model class.
+ *
+ *
+ *           This file was generated using Mongoose's C++ 
+ *           template generator script.  <mongoose@icculus.org>
+ *
+ *-- History ------------------------------------------------ 
+ *
+ * 2005.07.12:
+ * Mongoose - Created
+ ==========================================================================*/
+
+
+#ifndef GUARD__FREYJA_MONGOOSE_FREYJABONEABI_H_
+#define GUARD__FREYJA_MONGOOSE_FREYJABONEABI_H_
+
+#include "freyja.h"
+
+extern "C" {
+
+	///////////////////////////////////////////////////////////////////////
+	// BONE
+	///////////////////////////////////////////////////////////////////////
+
+	byte freyjaIsBoneAllocated(index_t boneIndex);
+	/*------------------------------------------------------
+	 * Pre  : 
+	 * Post : 
+	 ------------------------------------------------------*/
+
+	index_t freyjaBoneCreate(index_t skeletonIndex);
+	/*------------------------------------------------------
+	 * Pre  : 
+	 * Post : Creates a new rest bone for skeleton
+	 *        Returns valid Index or -1 on Error
+	 ------------------------------------------------------*/
+
+	void freyjaBoneDelete(index_t boneIndex);
+	/*------------------------------------------------------
+	 * Pre  : 
+	 * Post : 
+	 ------------------------------------------------------*/
+
+	void freyjaBoneFlags(index_t boneIndex, byte flags);
+	/*------------------------------------------------------
+	 * Pre  : 
+	 * Post : Set bone flags
+	 ------------------------------------------------------*/
+
+	void freyjaBoneParent(index_t boneIndex, index_t parentIndex);
+	/*------------------------------------------------------
+	 * Pre  : Bone exists
+	 * Post : Set bone parent
+	 *
+	 *        This doesn't affect skeleton, a follow up
+	 *        call to freyjaBoneAddChild1i is needed after
+	 *        all bones in skeleton are allocated
+	 ------------------------------------------------------*/
+
+	void freyjaBoneName(index_t boneIndex, const char *name);
+	/*------------------------------------------------------
+	 * Pre  : 
+	 * Post : Set human readable bone name
+	 ------------------------------------------------------*/
+
+	void freyjaBoneRemoveMesh(index_t boneIndex, index_t meshIndex);
+	void freyjaBoneAddMesh(index_t boneIndex, index_t meshIndex);
+	/*------------------------------------------------------
+	 * Pre  : freyjaBegin(FREYJA_BONE);
+	 * Post : Mesh is added to Bone's child list
+	 *
+	 *        Either makes mesh tree connection or
+	 *        simulates by vertex weights and pivots
+	 ------------------------------------------------------*/
+
+	void freyjaBoneRemoveChild(index_t boneIndex, index_t childIndex);
+	void freyjaBoneAddChild(index_t boneIndex, index_t childIndex);
+	/*------------------------------------------------------
+	 * Pre  : freyjaBegin(FREYJA_BONE);
+	 * Post : Child is added to Bone's child list
+	 ------------------------------------------------------*/
+
+	void freyjaBoneTranslate3f(index_t boneIndex, vec_t x, vec_t y, vec_t z);
+	void freyjaBoneTranslate3fv(index_t boneIndex, vec3_t xyz);
+	/*------------------------------------------------------
+	 * Pre  : freyjaBegin(FREYJA_BONE);
+	 * Post : Set bone relative position
+	 ------------------------------------------------------*/
+
+	void freyjaBoneRotateEuler3f(index_t boneIndex, vec_t p, vec_t h, vec_t r);
+	void freyjaBoneRotateEuler3fv(index_t boneIndex, vec3_t phr);
+	void freyjaBoneRotateQuat4f(index_t boneIndex,
+								vec_t w, vec_t x, vec_t y, vec_t z);
+	void freyjaBoneRotateQuat4fv(index_t boneIndex, vec4_t wxyz);
+	/*------------------------------------------------------
+	 * Pre  : freyjaBegin(FREYJA_BONE);
+	 * Post : Set bone orientation
+	 ------------------------------------------------------*/
+
+	const char *freyjaGetBoneName1s(index_t boneIndex);
+	/*------------------------------------------------------
+	 * Pre  : 
+	 * Post : 
+	 ------------------------------------------------------*/
+
+	void freyjaGetBoneName(index_t boneIndex, uint32 size, char *name);
+	/*------------------------------------------------------
+	 * Pre  : <name> must be allocated to <size> width
+	 *        A <size> of 64 is recommended
+	 *
+	 * Post : Gets bone[index]'s name as '\0' terminated string
+	 *        Returns FREYJA_PLUGIN_ERROR on error
+	 ------------------------------------------------------*/
+
+	index_t freyjaGetBoneParent(index_t boneIndex);
+	/*------------------------------------------------------
+	 * Pre  : 
+	 * Post : Returns bone[index]'s parent id
+	 ------------------------------------------------------*/
+
+	void freyjaGetBoneRotation4fv(index_t boneIndex, vec4_t wxyz);
+	/*------------------------------------------------------
+	 * Pre  : 
+	 * Post : Gets bone[index]'s orientation as a Quaternion
+	 *        Returns FREYJA_PLUGIN_ERROR on error
+	 ------------------------------------------------------*/
+
+	void freyjaGetBoneRotation3fv(index_t boneIndex, vec3_t phr);
+	/*------------------------------------------------------
+	 * Pre  : 
+	 * Post : Gets bone[index]'s orientation in Euler angles
+	 ------------------------------------------------------*/
+
+	void freyjaGetBoneTranslation3fv(index_t boneIndex, vec3_t xyz);
+	/*------------------------------------------------------
+	 * Pre  : 
+	 * Post : Gets bone[index]'s position
+	 ------------------------------------------------------*/
+
+	index_t freyjaGetBoneSkeletonIndex(index_t boneIndex);
+	/*------------------------------------------------------
+	 * Pre  : 
+	 * Post : Returns 'parent' skeletal index of this bone
+	 ------------------------------------------------------*/
+
+	index_t freyjaGetBoneChild(index_t boneIndex, uint32 element);
+	/*------------------------------------------------------
+	 * Pre  : 
+	 * Post : Iterator for bone children
+	 ------------------------------------------------------*/
+
+	uint32 freyjaGetBoneChildCount(index_t boneIndex);
+	/*------------------------------------------------------
+	 * Pre  : 
+	 * Post : Count of bone children
+	 ------------------------------------------------------*/
+
+}
+
+#endif
