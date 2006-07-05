c8d06d1f9b921b8d24da17424fcc354f552c0b1b
Terry Hendrix II <thendrix@users.noreply.github.com>
2006-07-05 06:12:46 +0000

    build backport

---
+++ bin/runme.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Local libs used to run local bin tests
+TEST=test
+SELECT=debug
+
+mkdir -p ${TEST}
+export LD_LIBRARY_PATH=${TEST}
+
+cp -f lib*/${SELECT}/*.so ${TEST}
+
+ldd ./freyja/${SELECT}/freyja | grep ${TEST}
+
+export LD_LIBRARY_PATH=${TEST}
+$@ ./freyja/${SELECT}/freyja
+
