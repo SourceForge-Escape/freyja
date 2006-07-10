72f72b090022d94546f1dc8e210d6882c43df6e9
mongoose <mongoose@607110db-3bd9-0310-8033-d7c559c36a9d>
2006-07-10 04:52:56 +0000

    assert update

---
+++ branches/freyja_0.9.5/libfreyja/gdb-freyjautil.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Local libs used to run local bin tests
+TEST=../bin/test
+SELECT=debug
+BIN=../bin/tools/test/freyjautil.test
+
+mkdir -p ${TEST}
+export LD_LIBRARY_PATH=${TEST}
+
+cp -f ../bin/lib*/${SELECT}/*.so ${TEST}
+
+ldd ${BIN} | grep ${TEST}
+
+gdb --args ${BIN} $@ 
+
